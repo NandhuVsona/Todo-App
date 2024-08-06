bbd06a0c09c3ac7bbdafac6735a1b1b2ea1444db
NandhuVsona <naveenvsona@gmail.com>
2024-08-06 20:51:44 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,13 @@ const app = express();
 const ejs = require("ejs");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const compression = require("compression");
+const noCache = require('nocache')
 
 //Middelwares
 app.use(express.json());
+app.use(compression());
+app.use(noCache())
 app.set("view-engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
